4cac38d650c15fbe851359b6b073d049e7834211
Madu01 <m4dud01@gmail.com>
2025-10-18 10:20:38 -0300

    correcoes

--- codigos para extração de dados/apitest.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-url = "https://servicodados.ibge.gov.br/api/v3/agregados"
-response = requests.get(url)
-print("Status:", response.status_code)
-print("First 1000 chars:\n", response.text[:1000])
