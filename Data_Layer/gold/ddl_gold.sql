d0849a214fe87c7fdcfac7dca3ab2ffd2832fc41
Madu01 <m4dud01@gmail.com>
2025-11-27 23:59:48 -0300

    add correcao no ddl

--- Data_Layer/gold/ddl_gold.sql
+++ Data_Layer/gold/ddl_gold.sql
@@ -1,96 +1,46 @@
-CREATE DATABASE POKEBI;
+CREATE SCHEMA IF NOT EXISTS dw;
 
-CREATE TABLE IF NOT EXISTS pokemon (
-    SRK_pok INT NOT NULL PRIMARY KEY,
-    pokemon_name VARCHAR(50) NOT NULL,
-    type_1 VARCHAR(50) NOT NULL,
-    type_2 VARCHAR(50),
-    height DOUBLE PRECISION NOT NULL,
-    weight DOUBLE PRECISION NOT NULL,
-    generation INT NOT NULL,
-    legendary BOOLEAN NOT NULL
+CREATE TABLE IF NOT EXISTS dw.Dim_pkm (
+    SRK_pkn SERIAL PRIMARY KEY,
+    pkm_nam VARCHAR(50) NOT NULL,
+    tp1 VARCHAR(50) NOT NULL,
+    gen INT NOT NULL,
+    leg BOOLEAN NOT NULL,
+    all_frm BOOLEAN NOT NULL,
+    glr_frm BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Dim_Form (
-    SRK_frm SERIAL NOT NULL PRIMARY KEY,
-    mega_evolution BOOLEAN NOT NULL,
-    alolan_form BOOLEAN NOT NULL,
-    galarian_form BOOLEAN NOT NULL,
-    forms_switchable BOOLEAN NOT NULL
+CREATE TABLE IF NOT EXISTS dw.Dim_btl (
+    SRK_btl SERIAL PRIMARY KEY,
+    atk INT NOT NULL,
+    dfs INT NOT NULL,
+    cap_rte INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS pokemon (
-    SRK_btl SERIAL NOT NULL PRIMARY KEY,
-    attack INT NOT NULL,
-    defense INT NOT NULL,
-    capture_rate INT NOT NULL,
-    base_experience INT NOT NULL,
-    exp_type VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS dw.Dim_efetContr (
+    SRK_eft SERIAL PRIMARY KEY,
+    agt_nrm DOUBLE PRECISION NOT NULL,
+    agt_fgt DOUBLE PRECISION NOT NULL,           
+    agt_rck DOUBLE PRECISION NOT NULL,
+    agt_drg DOUBLE PRECISION NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS pokemon (
-    SRK_efctr SERIAL NOT NULL PRIMARY KEY,
-    against_normal DOUBLE PRECISION NOT NULL,
-    against_fire DOUBLE PRECISION NOT NULL,
-    against_water DOUBLE PRECISION NOT NULL,
-    against_electric DOUBLE PRECISION NOT NULL,
-    against_grass DOUBLE PRECISION NOT NULL,
-    against_ice DOUBLE PRECISION NOT NULL,
-    against_fighting DOUBLE PRECISION NOT NULL,
-    against_poison DOUBLE PRECISION NOT NULL,
-    against_ground DOUBLE PRECISION NOT NULL,
-    against_flying DOUBLE PRECISION NOT NULL,
-    against_psychic DOUBLE PRECISION NOT NULL,
-    against_bug DOUBLE PRECISION NOT NULL,
-    against_rock DOUBLE PRECISION NOT NULL,
-    against_ghost DOUBLE PRECISION NOT NULL,
-    against_dragon DOUBLE PRECISION NOT NULL,
-    against_dark DOUBLE PRECISION NOT NULL,
-    against_steel DOUBLE PRECISION NOT NULL,
-    against_fairy DOUBLE PRECISION NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS possui (
-    SRK_pok_FK INT NOT NULL,
-    SRK_frm_FK INT NOT NULL,
+CREATE TABLE IF NOT EXISTS dw.Fat_pokdx (
+    SRK_pkx SERIAL PRIMARY KEY,
+    SRK_pkn INT NOT NULL,
+    SRK_btl INT NOT NULL,
+    SRK_eft INT NOT NULL,
     
-    PRIMARY KEY (SRK_pok_FK, SRK_frm_FK),
-    
-    FOREIGN KEY (SRK_pok_FK) 
-        REFERENCES Fat_pokemon(SRK_pok) 
-        ON DELETE CASCADE ON UPDATE CASCADE,
+    -- chaves estrangeiras
+    CONSTRAINT fk_pokemon
+        FOREIGN KEY (SRK_pkn)
+        REFERENCES dw.Dim_pkm (SRK_pkn),
         
-    FOREIGN KEY (SRK_frm_FK) 
-        REFERENCES Dim_Form(SRK_frm) 
-        ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS realiza_ (
-    SRK_pok_FK INT NOT NULL,
-    SRK_btl_FK INT NOT NULL,
-    
-    PRIMARY KEY (SRK_pok_FK, SRK_btl_FK),
-    
-    FOREIGN KEY (SRK_pok_FK) 
-        REFERENCES Fat_pokemon(SRK_pok) 
-        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_batalha
+        FOREIGN KEY (SRK_btl)
+        REFERENCES dw.Dim_btl (SRK_btl),
         
-    FOREIGN KEY (SRK_btl_FK) 
-        REFERENCES Dim_Btl(SRK_btl) 
-        ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS contem (
-    SRK_pok_FK INT NOT NULL,
-    SRK_efctr_FK INT NOT NULL,
-    
-    PRIMARY KEY (SRK_pok_FK, SRK_efctr_FK),
-    
-    FOREIGN KEY (SRK_pok_FK) 
-        REFERENCES Fat_pokemon(SRK_pok) 
-        ON DELETE CASCADE ON UPDATE CASCADE,
-        
-    FOREIGN KEY (SRK_efctr_FK) 
-        REFERENCES Dim_EfctContr(SRK_efctr) 
-        ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_efetividade
+        FOREIGN KEY (SRK_eft)
+        REFERENCES dw.Dim_efetContr (SRK_eft)
 );
